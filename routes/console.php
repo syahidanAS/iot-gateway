7a022fdeed23ddd3c2e4339f7a56c353d3541178
syahidanAS <syahidan139@gmail.com>
2026-05-02 00:43:24 +0700

    hotfix 3

--- routes/console.php
+++ routes/console.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+// use Illuminate\Foundation\Inspiring;
+// use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('automation:run')->everyFiveSeconds();
+Schedule::command('automation:run')->everyFiveMinutes();
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote');
